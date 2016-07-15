ec45142031e42b66ebcf1510cbfff78da968b81f
ee5415 <ee5415>
2016-07-15 12:01:34 +0800

    Initial commit

---
+++ app/src/main/java/com/example/android/scorecounting/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.android.scorecounting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    int bet_1 = 0;
+    int bet_2 = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
